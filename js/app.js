d1d80c14ef049bcb924ba0d195898999aa21108c
Maria <mmorrison1978@gmail.com>
2016-10-23 22:29:14 -0600

    added at route

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
 var app = angular.module("myApp", ['ngRoute']);
-app.config(function($locationProvider, $routeProvider){
-  $locationProvider.html5Mode(true);
+app.config(function($locationProvider, $routeProvider) {
+ $locationProvider.html5Mode(true);
 
-  $routeProvider
-    .when('/archer', {
-      templateUrl: '../partials/archer.html',
-      controller: 'MainController'
-    })
-    .when('/bojack', {
-      templateUrl: '../partials/bojack.html',
-      controller: 'MainController'
-    })
-    .when('/futurama', {
-      templateUrl: '../partials/futurama.html',
-      controller: 'MainController'
-    })
-    .when('/randm', {
-      templateUrl: '../partials/randm.html',
-      controller: 'MainController'
-    })
-    .when('/vbros', {
-      templateUrl: '../partials/vbros.html',
-      controller: 'MainController'
-    })
+ $routeProvider
+  .when('/archer', {
+   templateUrl: '../partials/archer.html',
+   controller: 'MainController'
+  })
+  .when('/bojack', {
+   templateUrl: '../partials/bojack.html',
+   controller: 'MainController'
+  })
+  .when('/futurama', {
+   templateUrl: '../partials/futurama.html',
+   controller: 'MainController'
+  })
+  .when('/randm', {
+   templateUrl: '../partials/randm.html',
+   controller: 'MainController'
+  })
+  .when('/vbros', {
+   templateUrl: '../partials/vbros.html',
+   controller: 'MainController'
+  })
+  .when('/at', {
+   templateUrl: '../partials/adventureTime.html',
+   controller: 'MainController'
+  })
 });
