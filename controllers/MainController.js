0f6ea38d2b79dd6a3aa05b4df823a0fc6c4bbc45
Maria <mmorrison1978@gmail.com>
2016-10-23 21:31:09 -0600

    at wired up

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,7 +1,8 @@
 app.controller('MainController', ['$scope', '$routeParams', function($scope, $routeParams) {
-  $scope.archer = ' Archer MainController is wired up'
-  $scope.bojack = ' Bojack MainController is wired up'
-  $scope.randm = ' R & M MainController is wired up'
-  $scope.vbros = ' V. Bros MainController is wired up'
-  $scope.futurama = "futurama MainController is wired up"
+ $scope.archer = ' Archer MainController is wired up'
+ $scope.bojack = ' Bojack MainController is wired up'
+ $scope.randm = ' R & M MainController is wired up'
+ $scope.vbros = ' V. Bros MainController is wired up'
+ $scope.futurama = 'futurama MainController is wired up'
+ $scope.at = 'Adventure Time is wired up'
 }]);
