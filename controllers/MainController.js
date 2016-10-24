768d20ae91ce0b2687acf2d48b0ac756736888be
Maria <mmorrison1978@gmail.com>
2016-10-23 22:25:54 -0600

    updated

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -4,5 +4,5 @@ app.controller('MainController', ['$scope', '$routeParams', function($scope, $ro
  $scope.randm = ' R & M MainController is wired up'
  $scope.vbros = ' V. Bros MainController is wired up'
  $scope.futurama = 'futurama MainController is wired up'
- $scope.at = 'Adventure Time is wired up'
+ $scope.adventureTime = 'Adventure Time is wired up'
 }]);
